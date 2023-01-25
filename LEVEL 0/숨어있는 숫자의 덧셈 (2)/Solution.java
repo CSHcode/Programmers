ca8b51b3107734368f248bf3e7254acdebeae3bc
CSHcode <116073413+CSHcode@users.noreply.github.com>
2023-01-25 17:07:52 +0900

    Create Solution.java

---
+++ LEVEL 0/숨어있는 숫자의 덧셈 (2)/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;                                     //모든 숫자를 더할 변수
+        String[] split=my_string.split("[a-zA-z]+");        //매개변수로 들어온 문자열을 대소문자 알파벳을 기준으로 스플릿
+        for(int i=0; i<split.length; i++) {
+				if(split[i].matches("[0-9]+")) {            //배열의 i번째 값이 정수면
+					answer+=Integer.parseInt(split[i]);     //answer에 더함
+				}
+			}
+        return answer;
+    }
+}
