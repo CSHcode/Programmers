bfec257dea87daa390e5ac33a4468071cd62357a
Sung Hwan Cho <116073413+CSHcode@users.noreply.github.com>
2023-02-27 12:11:46 +0900

    Create Solution.java

---
+++ LEVEL 0/다음에 올 숫자/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] common) {
+        int answer=0;
+        if(common[2]-common[1]==common[1]-common[0]){
+            answer=common[common.length-1]+common[1]-common[0];
+        }else{
+            answer=common[common.length-1]*common[1]/common[0];
+        }
+        return answer;
+    }
+}
